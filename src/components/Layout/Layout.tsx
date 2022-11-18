ddb66603b414a6358e7b0f6034b8fbe7782f8882
Alexandr Dimov <dalexandrd@gmail.com>
2022-11-18 23:18:26 +0200

    using ebs layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,75 @@
-import { PropsWithChildren } from "react";
-import { Sidebar, Topbar } from "components";
+import { Button } from 'components';
+import { Layout, AvatarInline, Sidebar, Icon } from 'ebs-design';
+import { useNavigate } from 'react-router-dom';
+import { api } from 'api';
+import { useUserContext } from 'context/userContext';
 
-export const Layout: React.FC<PropsWithChildren> = (props) => {
+import './Layout.scss';
+
+const MainLayout = (props: any) => {
+  const navigate = useNavigate();
+  const { name, surname, role } = useUserContext();
+
+  const handleLogout = () => {
+    api.users.logoutUser();
+    navigate('/login');
+  };
+
   return (
-    <div className="layout">
-      <Topbar />
-      <Sidebar />
-      <div className="page-content-wrapper">{props.children}</div>
-    </div>
+    <Layout>
+      <Layout.Topbar className="topbar">
+        <Layout.Topbar.LeftSide>
+          <div className="topbar__logo">
+            <img
+              onClick={() => navigate('/')}
+              src="/logo.png"
+              height="60px"
+              alt="logo"
+            />
+          </div>
+        </Layout.Topbar.LeftSide>
+
+        <Layout.Topbar.RightSide>
+          <AvatarInline
+            className="topbar__avatar"
+            alt={`${name} ${surname}`}
+            description={`${role}`}
+            status="active"
+            type="regular"
+            size="small"
+          />
+          <Button
+            onClick={handleLogout}
+            children={'logout'}
+            variant={'danger'}
+            size={'small'}
+          />
+        </Layout.Topbar.RightSide>
+      </Layout.Topbar>
+
+      <Sidebar>
+        <Sidebar.TopMenu>
+          <Sidebar.Item
+            onClick={() => navigate('/')}
+            prefix={<Icon type="users" />}
+            text="Our team"
+          />
+          <Sidebar.Item
+            onClick={() => navigate('/posts')}
+            prefix={<Icon type="message" />}
+            text="See all posts"
+          />
+          <Sidebar.Item
+            onClick={() => navigate('/dashboard')}
+            prefix={<Icon type="chart" />}
+            text="Dashboard"
+          />
+        </Sidebar.TopMenu>
+      </Sidebar>
+
+      <Layout.Content>{props.children} </Layout.Content>
+    </Layout>
   );
 };
+
+export { MainLayout as Layout };
